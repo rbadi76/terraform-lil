2aab2853c8cb60ed6b053164ca3cc1259654c6d0
badiTorg <badi@365.is>
2019-10-03 14:56:23 +0000

    Added an AWS server

--- subnetworks.tf
+++ subnetworks.tf
@@ -1,12 +1,3 @@
-resource "aws_vpc" "environment-example-two" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true
-    tags = {
-        Name = "terraform-aws-vpc-example-two"
-    }
-}
-
 resource "aws_subnet" "subnet1" {
     cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
     vpc_id = "${aws_vpc.environment-example-two.id}"
